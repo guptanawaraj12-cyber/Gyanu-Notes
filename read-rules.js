ffe8a2581c5acc3bc3cbce91278da32ff049b887
Gyanu Notes <guptanawaraj12@gmail.com>
2026-09-04 04:04:41 -0700

    remove stray read-rules.js helper

--- read-rules.js
+++
@@ -1,5 +0,0 @@
-const c = require('fs').readFileSync('d:/gyanu note code/firestore.rules', 'utf8');
-const lines = c.split('\n');
-for (let i = 12; i < 22; i++) {
-  console.log((i + 1) + '| ' + lines[i]);
-}
